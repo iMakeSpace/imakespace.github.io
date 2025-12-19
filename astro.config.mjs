3134f35c7645ed0bb75d0c30155370e8438c4dc7
dpfy <88242667+dpfy@users.noreply.github.com>
2025-12-20 01:02:41 +0800

    移除站点 URL 配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://imake.space',
 	integrations: [
 		starlight({
 			title: '云朵创客空间',
