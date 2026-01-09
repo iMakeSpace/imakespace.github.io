53617006f0a1f9a380157c0302e75b67aaff2faf
dpfy <88242667+dpfy@users.noreply.github.com>
2026-01-09 12:41:54 +0800

    add new page

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,18 +8,24 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: '云朵创客空间',
+			locales: {
+            root: {
+                label: '简体中文',
+                lang: 'zh-CN',
+        },
+      },
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/iMakeSpace' }],
 			sidebar: [
+				// {
+				// 	label: '指南',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: '课程介绍',
+					autogenerate: { directory: 'introduction' },
 				},
 			],
 		}),
